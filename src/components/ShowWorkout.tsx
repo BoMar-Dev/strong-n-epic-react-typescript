0802bde2618c4e1aa951ed8e067dea372a859cfd
marcus nilsson <marcusnilsson2010@gmail.com>
2023-09-07 12:03:05 +0200

    Added calender to screen, leeets go

--- src/components/ShowWorkout.tsx
+++ src/components/ShowWorkout.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Calendar from 'react-calendar';
 // Styles
 import "../styles/workout.css";
 
@@ -6,24 +7,33 @@ import "../styles/workout.css";
 import { IWorkout } from "../types/WorkoutType";
 import { Workouts } from "../db/Workouts";
 
+type ValuePiece = Date | null;
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
 
 const WorkoutList: React.FC = () => {
   const [count, setCount] = useState(0);
   const [workoutList, setWorkoutList] = useState<IWorkout[] | null> (null);
+  const [date, setDate] = useState(new Date());
   const [selectedWorkout, setSelectedWorkout] = useState<string[]>([]);
 
-
+  // (locale, date) => formatDate(date, 'dd')
 
   const increment = () => {
     setCount((count) => count + 1);
   };
-  
+
   const handledWorkoutChange = (workoutName: string) => {
     setSelectedWorkout([...selectedWorkout, workoutName]);
   }
 
 
+  const onChange = (date: any) => {
+    setDate(date)
+  }
+
   const renderWorkouts = () => {
+
     return Workouts.map((workout: IWorkout) => (
       <div className="workout-container" key={workout.id}>
         <h3>{workout.name}</h3>
@@ -46,9 +56,13 @@ const WorkoutList: React.FC = () => {
         <span>v</span>
       </div>
       <div>
-        <ul>
+        <ul className="selected-workout">
           <li>{selectedWorkout}</li>
         </ul>
+      </div>
+      <div className="calendar-container">
+      <Calendar onChange={onChange} value={date} />
+      {date.toString()}
       </div>
       <h1>Workout List</h1>
       {renderWorkouts()}
