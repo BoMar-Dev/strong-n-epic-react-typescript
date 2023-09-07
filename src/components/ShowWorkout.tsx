dca0abefc4652c474f4328e04073f0f852166efd
DaniellaKarlen <daniella.karlen@gmail.com>
2023-09-07 14:55:24 +0200

    calendar please pick a day

--- src/components/ShowWorkout.tsx
+++ src/components/ShowWorkout.tsx
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import Calendar from 'react-calendar';
+import Calendar from "react-calendar";
+
+// date-picker
+import { endOfWeek, isWithinInterval, startOfWeek, format } from "date-fns";
+import { DayPicker, Row, RowProps } from "react-day-picker";
+import "react-day-picker/dist/style.css";
+import { sv } from "date-fns/locale";
+
 // Styles
 import "../styles/workout.css";
 
@@ -10,9 +17,23 @@ import { IWorkout } from "../types/WorkoutType";
 // DB
 import { Workouts } from "../db/Workouts";
 
+function CurrentWeekRow(props: RowProps) {
+  const isDateInCurrentWeek = (dateToCheck: Date) => {
+    const today = new Date();
+    const start = startOfWeek(today);
+    const end = endOfWeek(today);
+    return isWithinInterval(dateToCheck, { start, end });
+  };
+  const isNotCurrentWeek = props.dates.every(
+    (date) => !isDateInCurrentWeek(date)
+  );
+  if (isNotCurrentWeek) return <></>;
+  return <Row {...props} />;
+}
 
 const WorkoutList: React.FC = () => {
-  const [calendar, setCalendar] = useState();
+  const [selectedDay, setSelectedDay] = useState<Date>();
+  // const [calendar, setCalendar] = useState();
   const [count, setCount] = useState(0);
   // const [workoutList, setWorkoutList] = useState<IWorkout[] | null> (null);
   const [date, setDate] = useState(new Date());
@@ -24,21 +45,29 @@ const WorkoutList: React.FC = () => {
 
   const handledWorkoutChange = (workoutName: string) => {
     setSelectedWorkout([...selectedWorkout, workoutName]);
-  }
-
+  };
 
   const onChange = (date: any) => {
-    setDate(date)
-  }
+    setDate(date);
+  };
+
+  const footer = selectedDay ? (
+    <p>You selected {format(selectedDay, "PPP")}.</p>
+  ) : (
+    <p>Please pick a day.</p>
+  );
 
   const renderWorkouts = () => {
-
     return Workouts.map((workout: IWorkout) => (
       <div className="workout-container" key={workout.id}>
         <h3>{workout.name}</h3>
         <p>Time: {workout.time}</p>
         <p>day: {workout.weekDay}</p>
-        <button onClick={() => {handledWorkoutChange(workout.name); increment()}}>
+        <button
+          onClick={() => {
+            handledWorkoutChange(workout.name);
+            increment();
+          }}>
           Boka
         </button>
       </div>
@@ -55,15 +84,27 @@ const WorkoutList: React.FC = () => {
       </div>
       <div>
         <ul className="selected-workout">
-          {selectedWorkout.map(item => (
+          {selectedWorkout.map((item) => (
             <li>{item}</li>
           ))}
           {/* <li>{selectedWorkout}</li> */}
         </ul>
       </div>
       <div className="calendar-container">
-      <Calendar onChange={onChange} value={date} />
-      {date.toString()}
+        <div>
+          <DayPicker
+            components={{ Row: CurrentWeekRow }}
+            showOutsideDays
+            mode="single"
+            selected={selectedDay}
+            onSelect={setSelectedDay}
+            footer={footer}
+            weekStartsOn={1}
+            locale={sv}
+          />
+        </div>
+        {/* <Calendar onChange={onChange} value={date} />
+        {date.toString()} */}
       </div>
       <h1>Workout List</h1>
       {renderWorkouts()}
